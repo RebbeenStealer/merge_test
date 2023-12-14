9bc6acaebc22f1c93884e3d09046bf9c67159196
RedbennStealer <ttuleula@gmail.com>
2023-12-14 17:39:24 +0900

    .

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Calculator {
+    // 학원에서 수정
     public static void main(String[] args) {
+	int my_add(int number1, int number2){
+	if (number1 < 0 or number2 < 0)
+		return;
+		return number1+number2;
+	}
     }
 }
